ea728426f7daed8f652ed181d82bd9a988529517
marcobusacca <marcobusacca01@gmail.com>
2023-06-01 17:22:34 +0200

    [SNACK-1] function checkRandomNumber

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -5,3 +5,27 @@ function generateRandomNumber(min, max){
 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
+// FUNZIONE CHE CONTROLLA CHE NON CI SIANO NUMERI RANDOM RINDONDANTI
+function checkRandomNumber(arrayNumberSeat){
+
+    // VARIABILE DI CONTROLLO
+    let checkNumber = false;
+
+    // VARIABILE CONTENENTE IL NUMERO CASUALE
+    let randomNumber;
+
+    while(checkNumber === false){
+
+        // RICHIAMO LA FUNZIONE CHE GENERA UN NUMERO RANDOM
+        randomNumber = generateRandomNumber(100, 999);
+
+        if(!arrayNumberSeat.includes(randomNumber)){ // CONTROLLO CHE IL NUMERO RANDOM GENERATO NON SIA RINDONDANTE
+            
+            checkNumber = true;
+        }
+    }
+
+    return randomNumber;
+}
