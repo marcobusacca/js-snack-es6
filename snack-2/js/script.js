a4c89d03fc39d052a6a7c77a972409e3fb8c2023
marcobusacca <marcobusacca01@gmail.com>
2023-06-02 14:53:12 +0200

    [SNACK-2] Dichiarazione studentsListArray

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,1 +1,68 @@
 "use strict";
+
+// DICHIARO L'ARRAY CON L'ELENCO DEGLI STUDENTI E LE LORO PROPRIETA'
+let studentsListArray = [
+
+    {
+
+        'id' : '213',
+
+        'name' : 'Marco della Rovere',
+
+        'grades' : '78',
+    },
+    
+    {
+
+        'id' : '110',
+
+        'name' : 'Paola Cortellessa',
+
+        'grades' : '96',
+    },
+
+    {
+
+        'id' : '250',
+
+        'name' : 'Andrea Mantegna',
+
+        'grades' : '48',
+    },
+
+    {
+
+        'id' : '145',
+
+        'name' : 'Gaia Borromini',
+
+        'grades' : '74',
+    },
+
+    {
+
+        'id' : '196',
+
+        'name' : 'Luigi Grimaldello',
+
+        'grades' : '68',
+    },
+
+    {
+
+        'id' : '102',
+
+        'name' : 'Piero della Francesca',
+
+        'grades' : '50',
+    },
+
+    {
+
+        'id' : '120',
+
+        'name' : 'Francesca da Polenta',
+
+        'grades' : '84',
+    }
+];
