b64431bef3817749ce1fbd78654b4fcff303b82e
marcobusacca <marcobusacca01@gmail.com>
2023-06-05 16:53:14 +0200

    [SNACK-3] racingBikeArray

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const racingBikeArray = [
+
+    {
+        'name'   : 'Alan',
+
+        'weight' : 8                // PESO ESPRESSO IN KG
+    },
+
+    {
+        'name'   : 'Atala',
+
+        'weight' : 9                // PESO ESPRESSO IN KG
+    },
+
+    {
+        'name'   : 'Basso',
+
+        'weight' : 6                // PESO ESPRESSO IN KG
+    },
+
+    {
+        'name'   : 'Bixxis',
+
+        'weight' : 7                // PESO ESPRESSO IN KG
+    },
+
+    {
+        'name'   : 'BH',
+
+        'weight' : 5                // PESO ESPRESSO IN KG
+    }
+];
