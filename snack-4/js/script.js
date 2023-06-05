c77c4341019c2e5aba5150e43eea9d2519c72905
marcobusacca <marcobusacca01@gmail.com>
2023-06-05 17:29:51 +0200

    [SNACK-4] squadreArray

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -6,3 +6,54 @@ function generateRandomNumber(min, max){
     // GENERO E RITORNO IL NUMERO RANDOM
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
+const squadreArray = [
+
+    {
+
+        'nome'          : 'Inter',
+
+        'puntiFatti'    : generateRandomNumber(1, 100),
+
+        'falliSubiti'   : generateRandomNumber(1, 100)
+    },
+
+    {
+
+        'nome'          : 'Milan',
+
+        'puntiFatti'    : generateRandomNumber(1, 100),
+
+        'falliSubiti'   : generateRandomNumber(1, 100)
+    },
+
+    {
+
+        'nome'          : 'Juventus',
+
+        'puntiFatti'    : generateRandomNumber(1, 100),
+
+        'falliSubiti'   : generateRandomNumber(1, 100)
+    },
+
+    {
+
+        'nome'          : 'Torino',
+
+        'puntiFatti'    : generateRandomNumber(1, 100),
+
+        'falliSubiti'   : generateRandomNumber(1, 100)
+    },
+
+    {
+
+        'nome'          : 'Napoli',
+
+        'puntiFatti'    : generateRandomNumber(1, 100),
+
+        'falliSubiti'   : generateRandomNumber(1, 100)
+    }
+];
+
+console.log(squadreArray)
