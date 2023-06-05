b1c4bd83ff68a948cd3a401979f05a03612b06e5
marcobusacca <marcobusacca01@gmail.com>
2023-06-05 17:07:06 +0200

    [SNACK-3] Snack Completato

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -35,3 +35,16 @@ const racingBikeArray = [
 
 // VARIABILE CHE CONTERRA' L'OGGETTO CON IL PESO MINORE
 let pesoMinore = racingBikeArray[0];
+
+// CICLO FOR EACH CHE ESTRAPOLA LE PROPRIETA' DELL'OGGETTO CON IL PESO MINORE
+racingBikeArray.forEach(({name, weight}) => {
+
+    if (weight < pesoMinore.weight){ // IL PESO DELL'OGGETTO ATTUALE E' MINORE ALL'OGGETTO CHE PRIMA AVEVA IL PESO MINORE
+
+        // ASSEGNO A PESO_MINORE LE PROPRIETA' DELL'OGGETTO CON IL PESO MINORE
+        pesoMinore = {name, weight}
+    }
+});
+
+// STAMPO IN CONSOLE LE PROPRIETA' DELL'OGGETTO CON IL PESO MINORE
+console.log(`La Bici da Corsa con il Peso Minore è : ${pesoMinore.name}, con un Peso di : ${pesoMinore.weight} KG!`);
