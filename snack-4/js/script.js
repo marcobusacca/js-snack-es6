e04b2d70c2ffade522e86c57caa1e9c82053c2c0
marcobusacca <marcobusacca01@gmail.com>
2023-06-05 17:25:01 +0200

    [SNACK-4] function generateRandomNumber

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// FUNZIONE CHE GENERA DEI NUMERI RANDOM
+function generateRandomNumber(min, max){
+
+    // GENERO E RITORNO IL NUMERO RANDOM
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
