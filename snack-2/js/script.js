7f04dbeb3f880b3cdc49fd475f6b2b928bb433f8
marcobusacca <marcobusacca01@gmail.com>
2023-06-05 09:53:12 +0200

    Esercizio Completato

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -5,65 +5,65 @@ let studentsListArray = [
 
     {
 
-        'id' : '213',
+        'id' : 213,
 
         'name' : 'Marco della Rovere',
 
-        'grades' : '78',
+        'grades' : 78
     },
     
     {
 
-        'id' : '110',
+        'id' : 110,
 
         'name' : 'Paola Cortellessa',
 
-        'grades' : '96',
+        'grades' : 96
     },
 
     {
 
-        'id' : '250',
+        'id' : 250,
 
         'name' : 'Andrea Mantegna',
 
-        'grades' : '48',
+        'grades' : 48
     },
 
     {
 
-        'id' : '145',
+        'id' : 145,
 
         'name' : 'Gaia Borromini',
 
-        'grades' : '74',
+        'grades' : 74
     },
 
     {
 
-        'id' : '196',
+        'id' : 196,
 
         'name' : 'Luigi Grimaldello',
 
-        'grades' : '68',
+        'grades' : 68
     },
 
     {
 
-        'id' : '102',
+        'id' : 102,
 
         'name' : 'Piero della Francesca',
 
-        'grades' : '50',
+        'grades' : 50
     },
 
     {
 
-        'id' : '120',
+        'id' : 120,
 
         'name' : 'Francesca da Polenta',
 
-        'grades' : '84',
+        'grades' : 84
     }
 ];
 
